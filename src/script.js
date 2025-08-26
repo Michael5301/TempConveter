f0d17b032140bd3cbaaf0cae544793c7944d5270
michael <maikogagi1@gmail.com>
2025-08-26 22:45:37 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,45 @@
+
+
+// Temp Conversion
+const inputBox = document.getElementById("inputBox");
+const toFarenhet = document.getElementById("toFarenhet");
+const toCelcius = document.getElementById("toCelcius");
+const results = document.getElementById("results");
+let temp;
+
+function tempConver() {
+    if (toFarenhet.checked) {
+        temp = ((inputBox.value * 9) / 5 + 32).toFixed(1);
+        results.textContent = `Temperaturature is ${temp}° Fahrenheit.`;
+        results.style.color = "black";
+        results.style.padding = "8px";
+    } else if (toCelcius.checked) {
+        temp = (((inputBox.value - 32) * 5) / 9).toFixed(1);
+        results.textContent = `Temperaturature is ${temp}° Celcius.`;
+        results.style.color = "black";
+        results.style.padding = "8px";
+    } else {
+        results.textContent = "Please select a uni to be converted";
+        results.style.color = "red";
+        results.style.padding = "8px";
+    }
+}
+
+// Calculator
+let display = document.getElementById("display");
+
+function appendTOdisplay(input) {
+    display.value += input;
+}
+
+function clearAll() {
+    display.value = "";
+}
+
+function calculate(params) {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error, try again!";
+    }
+}
